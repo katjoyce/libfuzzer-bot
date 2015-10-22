653b0b2c66fc1afbf0ad9f58a38ec173d63864ef
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-10-22 12:35:40 -0700

    refactor the loop_body for freetype fuzzer (#3). The size/time are small to allow quick testing.

---
+++ freetype/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2015 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+export PATH="$HOME/llvm-build/bin:$PATH"
+NAME=$1 # E.g. asan
+SAN=$2  # E.g. -fsanitize=address
+COV=$3  # E.g. -fsanitize-coverage=edge,8bit-counters
+
+(
+  rm -rf $NAME
+  cp -rf freetype2 $NAME
+  cd $NAME
+  ./autogen.sh
+  CC="clang  $SAN $COV"   ./configure
+  make -j
+)
+
+ln -sf $HOME/llvm/lib/Fuzzer .
+for f in Fuzzer/*cpp; do clang++ -std=c++11 -c $f -IFuzzer & done
+wait
+
+clang++ -std=c++11  $NAME/src/tools/ftfuzzer/ftfuzzer.cc \
+  $SAN $COV \
+  *.o -I $NAME/include -I . ${NAME}/objs/.libs/libfreetype.a  \
+  -lbz2 -lz -lpng -lharfbuzz  -o freetype2_${NAME}_fuzzer
