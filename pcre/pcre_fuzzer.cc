74393477cc091dd7f11f4467e922071bf33b2bf2
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-10-23 11:16:22 -0700

    [pcre] add regcomp flag

--- pcre/pcre_fuzzer.cc
+++ pcre/pcre_fuzzer.cc
@@ -11,7 +11,7 @@ extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
   regex_t preg;
   string str(reinterpret_cast<const char*>(data), size);
   string pat(str);
-  if (0 == regcomp(&preg, pat.c_str(), 0)) {
+  if (0 == regcomp(&preg, pat.c_str(), data[size/2] & ~REG_NOSUB)) {
     // regmatch_t pmatch[5];
     // regexec(&preg, s.c_str(), 5, pmatch, 0);
     regfree(&preg);
