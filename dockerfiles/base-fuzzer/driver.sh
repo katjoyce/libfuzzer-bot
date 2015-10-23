a6283d7b5223acbadaeb042be6591638d74b2f1b
Mike Aizatsky <mike.aizatsky@gmail.com>
2015-10-22 20:43:29 -0700

    Clear ASAN_OPTIONS for build.

--- dockerfiles/base-fuzzer/driver.sh
+++ dockerfiles/base-fuzzer/driver.sh
@@ -6,5 +6,10 @@ export CC="/work/llvm/bin/clang"
 export CXX="/work/llvm/bin/clang++"
 export PATH=/work/llvm/bin:$PATH
 
+OLD_ASAN_OPTIONS=$ASAN_OPTIONS
+# asan could get in the way of configure scripts.
+export ASAN_OPTIONS=""
 bash -x -e /src/scripts/build.sh
+
+export ASAN_OPTIONS=$OLD_ASAN_OPTIONS
 bash -x -e /src/scripts/run.sh
